205985c6e221c4fed8f8948e575d091887eeef63
Renz 8:28 <110266073+RenzKing@users.noreply.github.com>
2023-06-05 21:56:58 +0800

    jsx

--- src/index.js
+++ src/index.js
@@ -2,10 +2,32 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 const Greeting = () => {
-  return <h2>ReactJS Tutorial!</h2>;
+  return (
+    <div>
+      <div>
+        <h2>My First component</h2>
+        <ul>
+          <li>
+            <a href="#">Link1</a>
+          </li>
+          <li>
+            <a href="#">Link2</a>
+          </li>
+        </ul>
+      </div>
+      <h3>Header 3</h3>
+    </div>
+  );
 };
+
+// const Greeting = () => {
+//   return React.createElement(
+//     "div",
+//     {},
+//     React.createElement("h2", {}, "Hello world!")
+//   );
+// };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(<Greeting />);
-```
