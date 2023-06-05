752ca0a4653726be4aedacd7fa172117d36548d2
Renz 8:28 <110266073+RenzKing@users.noreply.github.com>
2023-06-05 22:15:21 +0800

    components

--- src/index.js
+++ src/index.js
@@ -1,24 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const Greeting = () => {
-  return (
-    <div>
-      <div>
-        <h2>My First component</h2>
-        <ul>
-          <li>
-            <a href="#">Link1</a>
-          </li>
-          <li>
-            <a href="#">Link2</a>
-          </li>
-        </ul>
-      </div>
-      <h3>Header 3</h3>
-    </div>
-  );
-};
+// const Greeting = () => {
+//   return (
+//     <>
+//       <h2>My First component</h2>
+//       <ul>
+//         <li>
+//           <a href="#">Link1</a>
+//         </li>
+//         <li>
+//           <a href="#">Link2</a>
+//         </li>
+//       </ul>
+//       <h3>Header 3</h3>
+//     </>
+//   );
+// };
 
 // const Greeting = () => {
 //   return React.createElement(
@@ -27,6 +25,20 @@ const Greeting = () => {
 //     React.createElement("h2", {}, "Hello world!")
 //   );
 // };
+
+const Greeting = () => {
+  return (
+    <>
+      <Person />
+      <Message />
+    </>
+  );
+};
+
+const Person = () => <h2>John Doe</h2>;
+const Message = () => {
+  return <p>This is my message</p>;
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
