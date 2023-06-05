90eabec12479190c0c97fc36106c43331e48a87d
Renz 8:28 <110266073+RenzKing@users.noreply.github.com>
2023-06-05 19:56:12 +0800

    REACTJS Tutorial

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 const Greeting = () => {
-  return <h2>Hello ReactJS!</h2>;
+  return <h2>ReactJS Tutorial!</h2>;
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
